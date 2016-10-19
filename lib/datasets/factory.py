05f9fce60319d053fa8b7ca9a21e7c3d833b67ae
Haoming Wang <haoming.exe@gmail.com>
2016-10-19 14:42:13 +0200

    Train Py-Faster-RCNN on Another Dataset

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -1,6 +1,7 @@
 # --------------------------------------------------------
 # Fast R-CNN
 # Copyright (c) 2015 Microsoft
+# Copyright (c) 2016 Haoming Wang
 # Licensed under The MIT License [see LICENSE for details]
 # Written by Ross Girshick
 # --------------------------------------------------------
@@ -11,6 +12,7 @@ __sets = {}
 
 from datasets.pascal_voc import pascal_voc
 from datasets.coco import coco
+from datasets.inria import inria
 import numpy as np
 
 # Set up voc_<year>_<split> using selective search "fast" mode
@@ -30,6 +32,11 @@ for year in ['2015']:
     for split in ['test', 'test-dev']:
         name = 'coco_{}_{}'.format(year, split)
         __sets[name] = (lambda split=split, year=year: coco(split, year))
+
+inria_devkit_path = '/home/ubuntu/code/py-faster-rcnn/data/INRIA_Person_devkit' # for AWS EC2
+for split in ['train', 'test']:
+    name = '{}_{}'.format('inria', split)
+    __sets[name] = (lambda split=split: inria(split, inria_devkit_path))
 
 def get_imdb(name):
     """Get an imdb (image database) by name."""
